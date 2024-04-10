ca46701bd243ce25eaad8c6f19be47712a15d65b
JosephO <jo101014wizcon@gmail.com>
2024-04-10 01:34:16 +0100

    registration and dashboard

---
+++ client/pages/dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container mx-auto">
+    <div class="w-1/2 mx-auto">
+      <div class="bg-white p-5 rounded-lg">
+        <h2 class="text-lg font-bold mb-4">Private Dashboard</h2>
+        <p class="text-gray-700">Welcome <span class="font-bold">{{ $auth.user.name }}</span></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: "auth"
+};
+</script>
---
+++ client/pages/register.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+
+    <div class="container mx-auto mt-5 md:w-1/2 lg:w-1/3">
+      <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h2 class="mb-6 text-xl font-bold">Sign up</h2>
+        <form @submit.prevent="register">
+          <div class="mb-4">
+            <label for="name" class="block text-gray-700 text-sm font-bold mb-2">First Name</label>
+            <input id="name" v-model="form.first_name" type="text"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required>
+          </div>
+          <div class="mb-4">
+            <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Last Name</label>
+            <input id="name" v-model="form.last_name" type="text"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required>
+          </div>
+          <div class="mb-4">
+            <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
+            <input id="email" v-model="form.email" type="email"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required>
+          </div>
+          <div class="mb-6">
+            <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+            <input id="password" v-model="form.password" type="password"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required>
+          </div>
+          <div class="flex items-center justify-between">
+            <button type="submit"
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Register</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: ['guest'],
+  data() {
+    return {
+      form: {
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: ''
+      }
+    };
+  },
+
+  methods: {
+    async register() {
+      try {
+        const response = await this.$axios.post('/register', {
+          data: this.form
+        });
+        console.log('Registration successful:', response);
+      } catch (error) {
+        console.error('Registration error:', error);
+      }
+    }
+  }
+};
+</script>
