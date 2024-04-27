333e0def7d2a81090e15a8b144a304ce0a546d5b
JosephO <jo101014wizcon@gmail.com>
2024-04-27 06:24:08 -0400

    dashboard test

--- client/pages/dashboard.vue
+++ client/pages/dashboard.vue
@@ -1,13 +1,29 @@
 <template>
-  <div class="container mx-auto">
-    <div class="w-1/2 mx-auto">
-      <div class="bg-white p-5 rounded-lg">
-        <h2 class="text-lg font-bold mb-4">Private Dashboard</h2>
-        <p class="text-gray-700">Welcome <span class="font-bold"></span></p>
+  <div class="container mx-auto p-6">
+    <h1 class="text-3xl font-bold mb-4">Dashboard</h1>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <!-- Card 1-->
+      <div class="bg-white rounded shadow p-6">
+        <h2 class="text-xl font-bold mb-2">Card 1</h2>
+        <p class="text-gray-700">This is some dummy data for Card 1.</p>
+      </div>
+
+      <!-- Card 2 -->
+      <div class="bg-white rounded shadow p-6">
+        <h2 class="text-xl font-bold mb-2">Card 2</h2>
+        <p class="text-gray-700">This is some dummy data for Card 2.</p>
+      </div>
+
+      <!-- Card 3 -->
+      <div class="bg-white rounded shadow p-6">
+        <h2 class="text-xl font-bold mb-2">Card 3</h2>
+        <p class="text-gray-700">This is some dummy data for Card 3.</p>
       </div>
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
